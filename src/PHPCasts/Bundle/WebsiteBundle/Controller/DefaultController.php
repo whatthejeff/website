b0de66174ed4f5cc37c020cc549db69c7e085d17
Daniel Ribeiro <drgomesp@gmail.com>
2014-02-18 00:54:00 -0300

    Finished basic persistence of simple proposal

--- src/PHPCasts/Bundle/WebsiteBundle/Controller/DefaultController.php
+++ src/PHPCasts/Bundle/WebsiteBundle/Controller/DefaultController.php
@@ -2,15 +2,17 @@
 
 namespace PHPCasts\Bundle\WebsiteBundle\Controller;
 
-use PHPCasts\Bundle\WebsiteBundle\Form\Type\ProposalType;
 use PHPCasts\Entity\Proposal;
+use PHPCasts\Bundle\WebsiteBundle\Form\Type\ProposalType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-        $proposalForm = $this->createForm(new ProposalType(), new Proposal());
+        $proposalForm = $this->createForm(new ProposalType(), new Proposal(), [
+            'action' => $this->generateUrl('php_casts_website_proposal_create'),
+        ]);
 
         return $this->render('PHPCastsWebsiteBundle:Default:index.html.twig', [
             'proposalForm' => $proposalForm->createView()
---
+++ src/PHPCasts/Bundle/WebsiteBundle/Controller/ProposalController.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * This file is part of the phpcasts package.
+ *
+ * (c) Daniel Ribeiro <drgomesp@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace PHPCasts\Bundle\WebsiteBundle\Controller;
+
+use PHPCasts\Entity\Proposal;
+use PHPCasts\Bundle\WebsiteBundle\Form\Type\ProposalType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class ProposalController extends Controller
+{
+    public function createAction()
+    {
+        $form = $this->createForm(new ProposalType(), $proposal = new Proposal());
+
+        try {
+            $submit = $form->handleRequest($this->getRequest());
+        } catch (\Exception $e) {
+            var_dump($e->getMessage()); exit;
+        }
+
+        $em = $this->get('doctrine.orm.entity_manager');
+
+        try {
+            $em->persist($proposal);
+            $em->flush();
+        } catch (\Exception $e) {
+            var_dump($e->getMessage()); exit;
+        }
+
+        return $this->redirect($this->generateUrl('php_casts_website_homepage'), Response::HTTP_CREATED);
+    }
+}
